c462b26fb78077722eac9078457c638d7552aab7
a-davlatov <davlatovamriddin7@gmail.com>
2022-11-27 20:21:41 +0500

    add smooth scroll

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ let lastScroll = 0;
 const headerEl = document.querySelector('#header');
 const introEl = document.querySelector('#intro');
 const burgerEl = document.querySelector('#burger');
+const diffuserEl = document.querySelector('#diffuser_video');
+const videoModalEl = document.querySelector('#modal_video');
+const videoModalCloseEl = document.querySelector('#modal-close');
 const bodyEl = document.body;
 let introElH = introEl.clientHeight;
 const defaultOffset = introElH;
@@ -32,7 +35,57 @@ burgerEl.addEventListener('click', (evt) => {
     evt.preventDefault();
     burgerEl.classList.toggle('clicked');
     headerEl.classList.toggle('show');
-    bodyEl.classList.toggle('show-nav');
+    bodyEl.classList.toggle('no-scroll');
+});
+
+// Smooth scroll
+const navLinks = document.querySelectorAll('[data-scroll]');
+navLinks.forEach((el) => {
+    el.addEventListener('click', (evt) => {
+        evt.preventDefault();
+
+        const elementId = el.dataset.scroll;
+        const elementOffset = document.getElementById(elementId).offsetTop;
+
+        burgerEl.classList.remove('clicked');
+        headerEl.classList.remove('show');
+        bodyEl.classList.remove('no-scroll');
+
+        if (window.innerWidth <= 414) {
+            window.scrollTo({
+                top: elementOffset - 56,
+                behavior: 'smooth'
+            });
+            return;
+        }
+
+        window.scrollTo({
+            top: elementOffset,
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+// Show modal
+diffuserEl.addEventListener('click', () => {
+    videoModalEl.classList.add('show');
+    bodyEl.classList.add('no-scroll');
+});
+
+videoModalCloseEl.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    videoModalEl.classList.remove('show');
+    bodyEl.classList.remove('no-scroll');
+});
+videoModalEl.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    videoModalEl.classList.remove('show');
+    bodyEl.classList.remove('no-scroll');
+});
+
+document.querySelector('.modal__dialog').addEventListener('click', (evt) => {
+    evt.stopPropagation();
 });
 
 window.addEventListener('load', () => {
